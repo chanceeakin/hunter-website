df0d6d101ff2d6050028af9f286db0e354b505d6
Chance <chance.eakin@gmail.com>
2016-12-10 08:50:15 -0600

    sorted calendar listings

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -15,8 +15,16 @@ $(document).ready(function () {
 		}
 		for (var i = 0; i < response.items.length; i++) {
 			console.log(response.items[i]);
-			$('#calendarFill').append(
-			'<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
+			console.log(response.items[i].description);
+			if (response.items[i].description === 'Evening') {
+				$('#evening').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
+				};
+			if (response.items[i].description === 'Matinee') {
+				$('#matinee').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
+				};
+			if (response.items[i].description === undefined || response.items[i].description === 'other') {
+				$('#calendarFill').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
+			}
 		}
 	});
 });
