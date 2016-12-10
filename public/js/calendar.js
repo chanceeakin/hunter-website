d22da1e0a39c13bab27f73911b8975f465033d4c
Chance <chance.eakin@gmail.com>
2016-12-10 17:30:14 -0600

    changing up how calendar data is accessed

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,30 +1,43 @@
-$(document).ready(function () {
-	$('#download-button').click(function () {
-	$('html, body').animate({
-		scrollTop: $('#scroll-here').offset().top
-	}, 2000);
-});
+function groupBy (array, f) {
+	var groups = {};
+	array.forEach((o) => {
+		var group = JSON.stringify(f(o));
+		groups[group] = groups[group] || [];
+		groups[group].push(o);
+	});
+	return Object.keys(groups).map((group) => {
+		return groups[group];
+	});
+}
+
+function consoleShit (item, index) {
+	for (var i = 0; i < item.length; i++) {
+		$('#calendarFill').append('<div class="col s12 m6 center calendar-ajax ' + item[i].summary + '"><div class="calendar-summary">' + item[i].summary + '</div><div class="calendar-date">' + moment(item[i].start.dateTime, moment.ISO_8601).format('MMM-DD-YYYY', 'en') + '</div><div class="calendar-time">' + moment(item[i].start.dateTime, moment.ISO_8601).format('hh:mm a') + '</div></div>');
+		$('.'+ item[i].summary).css("background-color", 'tomato');
+	}
+}
+
+$(document).ready(() => {
+	$('#download-button').click(() => {
+		$('html, body').animate({
+			scrollTop: $('#scroll-here').offset().top
+		}, 2000);
+	});
 	$.ajax({
 		url: '/api',
 		type: 'GET',
 		dataType: 'JSON'
 	}).done((response) => {
-		console.log('success');
 		if (!response.items) {
 			return;
 		}
-		for (var i = 0; i < response.items.length; i++) {
-			console.log(response.items[i]);
-			console.log(response.items[i].description);
-			if (response.items[i].description === 'Evening') {
-				$('#evening').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
-				};
-			if (response.items[i].description === 'Matinee') {
-				$('#matinee').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
-				};
-			if (response.items[i].description === undefined || response.items[i].description === 'other') {
-				$('#calendarFill').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format("MMM-DD-YYYY", 'en') + '</div><div class="calendar-time">'+ moment(response.items[i].start.dateTime, moment.ISO_8601).format("hh:mm a") +'</div></div>');
-			}
-		}
+		let operas = groupBy(response.items, (item) => {
+			return [item.summary];
+		});
+		console.log('operas', operas);
+		operas.forEach(consoleShit);
+			// console.log(moment(operas[i].start.dateTime, moment.ISO_8601).format('HH:mm'));
+			// if (response.items[i].description) {
+				// $('#calendarFill').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format('MMM-DD-YYYY', 'en') + '</div><div class="calendar-time">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format('hh:mm a') + '</div></div>');
 	});
 });
