3905811e6f138baf9e354567d8eac0d160958987
Chance <chance.eakin@gmail.com>
2016-12-11 19:12:21 -0600

    added cards to calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -10,13 +10,6 @@ function groupBy (array, f) {
 	});
 }
 
-function consoleShit (item, index) {
-	for (var i = 0; i < item.length; i++) {
-		$('#calendarFill').append('<div class="col s12 m6 center calendar-ajax ' + item[i].summary + '"><div class="calendar-summary">' + item[i].summary + '</div><div class="calendar-date">' + moment(item[i].start.dateTime, moment.ISO_8601).format('MMM-DD-YYYY', 'en') + '</div><div class="calendar-time">' + moment(item[i].start.dateTime, moment.ISO_8601).format('hh:mm a') + '</div></div>');
-		$('.'+ item[i].summary).css("background-color", 'tomato');
-	}
-}
-
 $(document).ready(() => {
 	$('#download-button').click(() => {
 		$('html, body').animate({
@@ -34,8 +27,13 @@ $(document).ready(() => {
 		let operas = groupBy(response.items, (item) => {
 			return [item.summary];
 		});
-		console.log('operas', operas);
-		operas.forEach(consoleShit);
+		for (let i = 0; i < operas.length; i++) {
+			$('#calendarFill').append('<div class="row" id="' + operas[i][0].summary + '"><div class="col s12 center"><h5 class="brown-text">' + operas[i][0].summary + '</h5></div></div>');
+			let activeID = operas[i];
+			for (let k = 0; k < activeID.length; k++) {
+				$('#' + activeID[k].summary).append('<div class="col s12 m4 center calendar-ajax ' + activeID[k].summary + '"><div class="card teal lighten-1 z-depth-2 text-black"><div class="card-content white-text"><span class="card-title">' + activeID[k].summary + '</span><p class="calendar-date">' + moment(activeID[k].start.dateTime, moment.ISO_8601).format('MMM-DD-YYYY', 'en') + '</p><p class="calendar-time">' + moment(activeID[k].start.dateTime, moment.ISO_8601).format('hh:mm a') + '</p><div class="card-action"><a class="left amber-text text-accent-1" href="' + activeID[k].description + '">Tickets</a></div></div></div>');
+			}
+		}
 			// console.log(moment(operas[i].start.dateTime, moment.ISO_8601).format('HH:mm'));
 			// if (response.items[i].description) {
 				// $('#calendarFill').append('<div class="calendar-ajax"><div class="calendar-summary">' + response.items[i].summary + '</div><div class="calendar-date">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format('MMM-DD-YYYY', 'en') + '</div><div class="calendar-time">' + moment(response.items[i].start.dateTime, moment.ISO_8601).format('hh:mm a') + '</div></div>');
